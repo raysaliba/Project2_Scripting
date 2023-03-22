50a6d3e1f2d684a747526617949f94b1087e5882
Raymond Saliba <raymondsaliba@Raymonds-MacBook-Pro.local>
2023-03-22 21:09:02 +0200

    added a function that checks for subdomains of a website

--- project2.py
+++ project2.py
@@ -1,12 +1,31 @@
 import re
 import requests
+import sys
 
 
 
 available = False;
+link = sys.argv[1]
 
 def check_website(link):
     response = requests.get(link)
 
     if response.status_code == 200:
         available = True
+def check_subdomain(link):
+    with open('subdomain_dictionary.bat') as f:
+        for line in f:
+            new_url= line+"."+link
+            response = requests.get(new_url)
+
+            if response.status_code == 200:
+                with open('sundomains_output.bat', 'a') as f:
+                        f.write(line)
+
+def check_directories(link):
+
+
+
+
+def check_files(link):
+   
